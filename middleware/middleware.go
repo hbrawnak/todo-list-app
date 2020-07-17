293e50d8aebce6066d13d2ee23765cdd7c93cd3d
Habibur Rahman <hbrawnak@gmail.com>
2020-07-18 02:18:22 +0600

    router setting, home and task api with prefix set

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -2,10 +2,16 @@ package middleware
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"log"
 )
 
 const connectionString = "mongodb://localhost:27017"
@@ -32,4 +38,44 @@ func init() {
 	collection = client.Database(database).Collection(collName)
 
 	fmt.Println("Connection instance created")
+}
+
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, `{"alive": true}`)
+}
+
+func GetAllTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "")
+	payload := getAllTask()
+	json.NewEncoder(w).Encode(payload)
+}
+
+func getAllTask() []primitive.M {
+	cur, err := collection.Find(context.Background(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var results []primitive.M
+
+	for cur.Next(context.Background()) {
+		var result bson.M
+
+		e := cur.Decode(result)
+		if e != nil {
+			log.Fatal(e)
+		}
+		//fmt.Println("cur..&gt;", cur, "result", reflect.TypeOf(result), reflect.TypeOf(result["_id"]))
+		results = append(results, result)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	cur.Close(context.Background())
+	return results
 }
--- router/router.go
+++ router/router.go
@@ -1,1 +1,16 @@
 package router
+
+import (
+	"../middleware"
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/", middleware.HealthCheckHandler).Methods("GET", "OPTIONS")
+
+	r := router.PathPrefix("/api").Subrouter()
+	r.HandleFunc("/task", middleware.GetAllTask).Methods("GET", "OPTIONS")
+
+	return router
+}
