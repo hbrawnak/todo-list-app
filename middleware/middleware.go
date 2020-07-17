f67ef0a26ae3ea2f3b607b15cbafd33844c0f7b5
Habibur Rahman <hbrawnak@gmail.com>
2020-07-18 00:29:06 +0600

    DB connection settings

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,35 @@
 package middleware
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+const connectionString = "mongodb://localhost:27017"
+const database = "todo"
+const collName = "todolist"
+
+var collection *mongo.Collection
+
+func init() {
+	clientOption := options.Client().ApplyURI(connectionString)
+	client, err := mongo.Connect(context.TODO(), clientOption)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB")
+
+	collection = client.Database(database).Collection(collName)
+
+	fmt.Println("Connection instance created")
+}
